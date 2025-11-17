3e176c83397b5539ebefc449f1fcb54f6b0ad6e6
keltttuo <81021904+keltttuo@users.noreply.github.com>
2025-11-17 09:30:24 +0200

    Refactor get_dimension to comment out unused cases
    Comment out unused rotation type cases in get_dimension method.

--- py3dbp/main.py
+++ py3dbp/main.py
@@ -40,16 +40,16 @@ class Item:
     def get_dimension(self):
         if self.rotation_type == RotationType.RT_WHD:
             dimension = [self.width, self.height, self.depth]
-        elif self.rotation_type == RotationType.RT_HWD:
-            dimension = [self.height, self.width, self.depth]
-        elif self.rotation_type == RotationType.RT_HDW:
-            dimension = [self.height, self.depth, self.width]
+        #elif self.rotation_type == RotationType.RT_HWD:
+        #    dimension = [self.height, self.width, self.depth]
+        #elif self.rotation_type == RotationType.RT_HDW:
+        #    dimension = [self.height, self.depth, self.width]
         elif self.rotation_type == RotationType.RT_DHW:
             dimension = [self.depth, self.height, self.width]
-        elif self.rotation_type == RotationType.RT_DWH:
-            dimension = [self.depth, self.width, self.height]
-        elif self.rotation_type == RotationType.RT_WDH:
-            dimension = [self.width, self.depth, self.height]
+        #elif self.rotation_type == RotationType.RT_DWH:
+        #    dimension = [self.depth, self.width, self.height]
+        #elif self.rotation_type == RotationType.RT_WDH:
+        #    dimension = [self.width, self.depth, self.height]
         else:
             dimension = []
 
